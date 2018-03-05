ec2d1a08cdffe2205b737dd97dbea95af5db0768
makQi <makqigoog@gmail.com>
2018-03-05 20:12:25 +0800

    verify.js_2018年3月5日20:12:04

--- PC/unpackage/verify.js
+++ PC/unpackage/verify.js
@@ -4,3 +4,18 @@
 		location.href = window.location.protocol + '//' + window.location.host;
 	}
 })();
+
+function newsfontColor() { // 新闻标题，字体颜色
+    var keyword = keyStr.split(' ');
+    $('.tableBox').find('.newsfont').each(function(index, el) {
+        var $this = $(this).children('a');
+        for (var i = 0; i < keyword.length; i++) {
+            if (keyword[i] == '') {
+                continue;
+            }
+            var re = new RegExp(keyword[i], 'g');
+            var _text = $this.html();
+            $this.html(_text.replace(re, '<span style="color: #53A7E3">' + keyword[i] + '</span>'));
+        }
+    });
+}
