18b8602f9dd1e3cf4b9ab9b8fdaf95d89b591ebe
makQi <makqigoog@gmail.com>
2018-03-10 21:04:03 +0800

    2018年3月10日21:03:57_nodeServer

---
+++ PC/templates/nodeServer/README.md
@@ -0,0 +1,7 @@
+## node本地WEB服务器搭建，文件结构。
+* dist文件夹为前端静态代码html、css、js等。
+* http.js为node服务器搭建代码。
+* mine.js可解析的文件类型。
+`node http.js`
+>启动服务
+
---
+++ PC/templates/nodeServer/http.js
@@ -0,0 +1,44 @@
+var PORT = 8000; //
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var mine = require('./mine').types; //
+var path = require('path');
+var projectsFolder = 'dist'; //这里是项目文件夹名称;
+
+var server = http.createServer(function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var realPath = path.join("dist", pathname);
+
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function(exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function(err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
+});
+server.listen(PORT);
+console.log("服务成功启动，Server runing at port: " + PORT + ".");
