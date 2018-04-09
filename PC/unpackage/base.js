cacbb177bd5b20ff7864f0e1d751a4ebb0bffac6
makQi <makQi@users.noreply.github.com>
2018-04-09 15:54:38 +0800

    Update base.js
    base.js_增加文档高度获取方法_2018年4月9日15:54:33

--- PC/unpackage/base.js
+++ PC/unpackage/base.js
@@ -191,6 +191,39 @@ function DocCookies() {
 
 
 function MakBaseFn() {
+    
+    /**
+     * 获取窗口滚动条卷曲的高度
+     */
+    this.getScrollTop = function() {
+        var scrollTop = 0;
+        if (document.documentElement && document.documentElement.scrollTop) {
+            scrollTop = document.documentElement.scrollTop;
+        } else if (document.body) {
+            scrollTop = document.body.scrollTop;
+        }
+        return scrollTop;
+    }
+    
+    /**
+     * 获取窗口可视范围的高度
+     */
+    this.getClientHeight = function() {
+        var clientHeight = 0;
+        if (document.body.clientHeight && document.documentElement.clientHeight) {
+            clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+        } else {
+            clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+        }
+        return clientHeight;
+    }
+    
+    /**
+     * 获取文档内容实际高度
+     */
+    this.getScrollHeight = function() {
+        return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+    }
 
     /**
      * 中文字符转unicode码
