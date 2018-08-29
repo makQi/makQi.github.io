b6ae4956be1990a35cfdb8d05150944487f4edfb
makQi <makQi@users.noreply.github.com>
2018-08-29 16:18:48 +0800

    base.js
    时间差方法修改

--- PC/unpackage/base.js
+++ PC/unpackage/base.js
@@ -421,26 +421,30 @@ function MakBaseFn() {
         var hour = minute * 60;
         var day = hour * 24;
         var month = day * 30;
+        var year = month * 12;
         var now = new Date().getTime();
         var diffValue = now - new Date(startTime).getTime();
         if (diffValue < 0) {
             return '本机时间有误';
         }
+        var yearC = diffValue / year;
         var monthC = diffValue / month;
         var weekC = diffValue / (7 * day);
         var dayC = diffValue / day;
         var hourC = diffValue / hour;
         var minC = diffValue / minute;
-        if (monthC >= 1) {
-            result = "" + parseInt(monthC) + "月前";
+        if (yearC >= 1) {
+            result = parseInt(yearC) + "年前";
+        } else if (monthC >= 1) {
+            result = parseInt(monthC) + "月前";
         } else if (weekC >= 1) {
-            result = "" + parseInt(weekC) + "周前";
+            result = parseInt(weekC) + "周前";
         } else if (dayC >= 1) {
-            result = "" + parseInt(dayC) + "天前";
+            result = parseInt(dayC) + "天前";
         } else if (hourC >= 1) {
-            result = "" + parseInt(hourC) + "小时前";
+            result = parseInt(hourC) + "小时前";
         } else if (minC >= 1) {
-            result = "" + parseInt(minC) + "分钟前";
+            result = parseInt(minC) + "分钟前";
         } else {
             result = "刚刚";
         }
