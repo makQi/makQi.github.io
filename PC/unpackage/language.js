f0f04aba75e316931d63b46480add4ec8dd2978f
金融科技部实习生15 <jrkj-shixi15@dev.efunds.com.cn>
2019-01-09 12:28:45 +0800

    语言包_2019年1月9日12:28:23

--- PC/unpackage/language.js
+++ PC/unpackage/language.js
@@ -4,7 +4,6 @@
     script.src = "http://www.microsoftTranslator.com/ajax/v3/WidgetV3.ashx?siteData=ueOIGRSKkd965FeEGM5JtQ**";
     document.getElementsByTagName('head')[0].appendChild(script);
 
-
     var value = sessionStorage.getItem("language");
     document.onreadystatechange = function() {
         if (document.readyState == 'complete') {
@@ -27,24 +26,11 @@
 
 var langArr = {
     search: ['搜索', 'search'],
-    ai: ['AI分析师', 'AI Analyst'],
-    equity: ['权益研究员', 'Equity researcher'],
-    risk: ['ESG风险排行', 'ESG Risk Ranking'],
-    decline: ['ESG跌幅排行', 'ESG Decline Ranking'],
-    industry: ['ESG行业平均分', 'ESG Industry Average Score'],
-    puton: ['展开全部内容', 'Expand All Content'],
-    totalSplit: ['总分拆分', 'Total Score Split'],
-    industrySplit: ['行业拆分', 'Industry Split'],
-    thirdParty: ['第三方点评', 'Third party reviews'],
-    companyReviews: ['公司点评', 'Company reviews'],
-    indicatorScore: ['指标得分', 'Indicator score'],
-    regulatoryInfo: ['监管信息', 'Regulatory information'],
-    newsAmp: ['新闻舆情', 'News & amp; public opinion'],
-    modifying: ['修改中', 'Modifying'],
     modify: ['修改', 'Modify'],
     save: ['保存', 'Save'],
     cancel: ['取消', 'Cancel'],
     company: ['公司', 'Company'],
+    date: ['日期', 'Date'],
     time: ['时间', 'Time'],
     updated: ['更新', 'Updated']
 };
